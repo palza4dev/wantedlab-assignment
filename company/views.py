70a6e3c78e7ac9b88f0f6ad4d050b4eaf7f85fcc
qhsdnr0 <qhsdnr0@gmail.com>
2021-11-09 20:39:33 +0900

    Modify : ProductView에 RelatedCompany 생성 추가
    - 모델링 수정에 따른 RelatedCompany 데이터 추가 생성

--- company/views.py
+++ company/views.py
@@ -1,22 +1,24 @@
 import json
 
+from itertools    import permutations
 from django.views import View
 from django.http  import JsonResponse
 from django.db    import transaction
 
-from .models      import Company, Tag, Language
+from .models      import Company, RelatedCompany, Tag, Language
 
 class CompanyView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            
+            related_company = []
             with transaction.atomic():
                 for language in data['company_name']:
                     languages, created = Language.objects.get_or_create(name=language)
                     company            = Company.objects.create(
                         name = data['company_name'][language]
                     )
+                    related_company.append(company.id)
                     languages.companylanguage_set.create(
                         company = company
                     )
@@ -28,7 +30,13 @@ class CompanyView(View):
                         tags.taglanguage_set.create(
                             language = languages
                         )
-                        
+
+                related_company_list = permutations(related_company, 2)
+                
+                [RelatedCompany.objects.create(
+                    company_id         = company_id[0],
+                    related_company_id = company_id[1]
+                ) for company_id in related_company_list]
 
             x_wanted_language = request.headers.get("x-wanted-language")
             
@@ -40,7 +48,7 @@ class CompanyView(View):
                 'tags'         : [tag['tag_name'][x_wanted_language] for tag in data['tags']]
             }
             
-            return JsonResponse({'result' : company_list}, status = 201)
+            return JsonResponse(company_list, status = 201)
         
         except KeyError:
             JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
