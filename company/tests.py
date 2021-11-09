82e40a0ffbd9ef61d6e06d11db5ec35bb7732da1
qhsdnr0 <qhsdnr0@gmail.com>
2021-11-10 00:56:59 +0900

    Add : CompanyView get method, tests.py
    - CompanyView get 자동완성 기능 구현
    
    - CompanyView post, get method, CompanySearchView get method에 대한 unittest 작성

--- company/tests.py
+++ company/tests.py
@@ -1,105 +1,277 @@
-import pytest
 import json
 
-from app import app
+from django.test import TestCase, Client
 
+from company.models import Company, CompanyLanguage, CompanyTag, Language, RelatedCompany, Tag, TagLanguage
 
-@pytest.fixture
-def api():
-    return app.test_client()
+class CompanyViewTest(TestCase):
+    def setUp(self):
+        Company.objects.create(
+            id = 1,
+            name = "주식회사 링크드코리아"
+        )
+        Company.objects.create(
+            id = 2,
+            name = "스피링크"
+        )
+        
+        Company.objects.create(
+            id = 3,
+            name = "원티드랩"
+        )
+        
+        Company.objects.create(
+            id = 4,
+            name = "Wantedlab"
+        )
+        
+        Tag.objects.create(
+            id = 1,
+            name = "태그_4"
+        )
+        
+        Tag.objects.create(
+            id = 2,
+            name = "태그_20"
+        )
+        
+        Tag.objects.create(
+            id = 3,
+            name = "태그_16"
+        )
+        
+        Tag.objects.create(
+            id = 4,
+            name = "tag_4"
+        )
+        
+        Tag.objects.create(
+            id = 5,
+            name = "tag_20"
+        )
+        
+        Tag.objects.create(
+            id = 6,
+            name = "tag_16"
+        )
+        
+        CompanyTag.objects.create(
+            id = 1,
+            company_id = 3,
+            tag_id = 1
+        )
+        
+        CompanyTag.objects.create(
+            id = 2,
+            company_id = 3,
+            tag_id = 2
+        )
+        
+        CompanyTag.objects.create(
+            id = 3,
+            company_id = 3,
+            tag_id = 3
+        )
+        
+        CompanyTag.objects.create(
+            id = 4,
+            company_id = 4,
+            tag_id = 4
+        )
+        
+        CompanyTag.objects.create(
+            id = 5,
+            company_id = 4,
+            tag_id = 5
+        )
+        
+        CompanyTag.objects.create(
+            id = 6,
+            company_id = 4,
+            tag_id = 6
+        )
+        
+        Language.objects.create(
+            id = 1,
+            name = 'ko',
+        )
+        
+        Language.objects.create(
+            id = 2,
+            name = 'en',
+        )
+        
+        CompanyLanguage.objects.create(
+            id = 1,
+            company_id = 1,
+            language_id = 1,
+        )
+        
+        CompanyLanguage.objects.create(
+            id = 2,
+            company_id = 2,
+            language_id = 1,
+        )
+        
+        CompanyLanguage.objects.create(
+            id = 3,
+            company_id = 3,
+            language_id = 1,
+        )
+        
+        CompanyLanguage.objects.create(
+            id = 4,
+            company_id = 4,
+            language_id = 2,
+        )
+        
+        TagLanguage.objects.create(
+            id = 1,
+            tag_id = 1,
+            language_id = 1
+        )
+        
+        TagLanguage.objects.create(
+            id = 2,
+            tag_id = 2,
+            language_id = 1
+        )
+        
+        TagLanguage.objects.create(
+            id = 3,
+            tag_id = 3,
+            language_id = 1
+        )
+        
+        TagLanguage.objects.create(
+            id = 4,
+            tag_id = 4,
+            language_id = 2
+        )
+        
+        TagLanguage.objects.create(
+            id = 5,
+            tag_id = 5,
+            language_id = 2
+        )
 
-
-def test_company_name_autocomplete(api):
-    """
-    1. 회사명 자동완성
-    회사명의 일부만 들어가도 검색이 되어야 합니다.
-    header의 x-wanted-language 언어값에 따라 해당 언어로 출력되어야 합니다.
-    """
-    resp = api.get("/search?query=링크", headers=[("x-wanted-language", "ko")])
-    searched_companies = json.loads(resp.data.decode("utf-8"))
-
-    assert resp.status_code == 200
-    assert searched_companies == [
-        {"company_name": "주식회사 링크드코리아"},
-        {"company_name": "스피링크"},
-    ]
-
-
-def test_company_search(api):
-    """
+        TagLanguage.objects.create(
+            id = 6,
+            tag_id = 6,
+            language_id = 2
+        )
+        
+        RelatedCompany.objects.create(
+            id = 1,
+            company_id = 3,
+            related_company_id = 4,
+        )
+        
+        RelatedCompany.objects.create(
+            id = 2,
+            company_id = 4,
+            related_company_id = 3,
+        )
+        
+    def tearDown(self):
+        CompanyLanguage.objects.all().delete()
+        CompanyTag.objects.all().delete()
+        TagLanguage.objects.all().delete()
+        Company.objects.all().delete()
+        Tag.objects.all().delete()
+        Language.objects.all().delete()
+        RelatedCompany.objects.all().delete()
+        
+    def test_company_name_autocomplete(self):
+        """
+        1. 회사명 자동완성
+        회사명의 일부만 들어가도 검색이 되어야 합니다.
+        header의 x-wanted-language 언어값에 따라 해당 언어로 출력되어야 합니다.
+        """
+        client = Client()
+        headers={"HTTP_x-wanted-language" : "ko"}
+        resp = client.get("/search?query=링크", **headers)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json(),
+           { 'searched_companies' : [{"company_name": "주식회사 링크드코리아"},
+            {"company_name": "스피링크"}],
+           })
+        
+    def test_company_search(self):
+        """
     2. 회사 이름으로 회사 검색
     header의 x-wanted-language 언어값에 따라 해당 언어로 출력되어야 합니다.
     """
-    resp = api.get(
-        "/companies/Wantedlab", headers=[("x-wanted-language", "ko")]
-    )
+        client = Client()
+        headers = {"HTTP_x-wanted-language" : "ko"}
+        resp = client.get("/companies/Wantedlab", **headers)
 
-    company = json.loads(resp.data.decode("utf-8"))
-    assert resp.status_code == 200
-    assert company == {
-        "company_name": "원티드랩",
-        "tags": [
-            "태그_4",
-            "태그_20",
-            "태그_16",
-        ],
-    }
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json(), 
+            {
+                "company_name": "원티드랩",
+                "tags": [
+                    "태그_4",
+                    "태그_20",
+                    "태그_16",
+                ],
+            })
 
-    # 검색된 회사가 없는경우 404를 리턴합니다.
-    resp = api.get(
-        "/companies/없는회사", headers=[("x-wanted-language", "ko")]
-    )
+        # 검색된 회사가 없는경우 404를 리턴합니다.
+        resp = client.get(
+            "/companies/없는회사", headers=[("x-wanted-language", "ko")]
+        )
 
-    assert resp.status_code == 404
-
-
-def test_new_company(api):
-    """
-    3.  새로운 회사 추가
-    새로운 언어(tw)도 같이 추가 될 수 있습니다.
-    저장 완료후 header의 x-wanted-language 언어값에 따라 해당 언어로 출력되어야 합니다.
-    """
-    resp = api.post(
-        "/companies",
-        json={
-            "company_name": {
-                "ko": "라인 프레쉬",
-                "tw": "LINE FRESH",
-                "en": "LINE FRESH",
-            },
-            "tags": [
-                {
-                    "tag_name": {
-                        "ko": "태그_1",
-                        "tw": "tag_1",
-                        "en": "tag_1",
-                    }
+        self.assertEqual(resp.status_code, 404)
+    
+    def test_new_company(self):
+        """
+        3.  새로운 회사 추가
+        새로운 언어(tw)도 같이 추가 될 수 있습니다.
+        저장 완료후 header의 x-wanted-language 언어값에 따라 해당 언어로 출력되어야 합니다.
+        """
+        client = Client()
+        
+        data={
+                "company_name": {
+                    "ko": "라인 프레쉬",
+                    "tw": "LINE FRESH",
+                    "en": "LINE FRESH",
                 },
-                {
-                    "tag_name": {
-                        "ko": "태그_8",
-                        "tw": "tag_8",
-                        "en": "tag_8",
+                "tags": [
+                    {
+                        "tag_name": {
+                            "ko": "태그_1",
+                            "tw": "tag_1",
+                            "en": "tag_1",
+                        }
+                    },
+                    {
+                        "tag_name": {
+                            "ko": "태그_8",
+                            "tw": "tag_8",
+                            "en": "tag_8",
+                        }
+                    },
+                    {
+                        "tag_name": {
+                            "ko": "태그_15",
+                            "tw": "tag_15",
+                            "en": "tag_15",
+                        }
                     }
-                },
-                {
-                    "tag_name": {
-                        "ko": "태그_15",
-                        "tw": "tag_15",
-                        "en": "tag_15",
-                    }
-                }
-            ]
-        },
-        headers=[("x-wanted-language", "tw")],
-    )
-
-    company = json.loads(resp.data.decode("utf-8"))
-    assert company == {
-        "company_name": "LINE FRESH",
-        "tags": [
-            "tag_1",
-            "tag_8",
-            "tag_15",
-        ],
-    }
+                ]
+            }
+        headers = {"HTTP_x-wanted-language" : "tw"}
+        resp = client.post("/companies", json.dumps(data), content_type='application/json', **headers)
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(resp.json(), 
+            {
+                "company_name": "LINE FRESH",
+                "tags": [
+                    "tag_1",
+                    "tag_8",
+                    "tag_15",
+                ],
+            }
+        )
--- company/views.py
+++ company/views.py
@@ -1,9 +1,10 @@
 import json
 
-from itertools    import permutations
-from django.views import View
-from django.http  import JsonResponse
-from django.db    import transaction
+from itertools              import permutations
+from django.views           import View
+from django.db.models       import Q
+from django.http            import JsonResponse
+from django.db              import transaction
 
 from .models      import Company, CompanyLanguage, RelatedCompany, Tag, Language
 
@@ -30,9 +31,7 @@ class CompanyView(View):
                         tags.taglanguage_set.create(
                             language = languages
                         )
-
                 related_company_list = permutations(related_company, 2)
-                
                 [RelatedCompany.objects.create(
                     company_id         = company_id[0],
                     related_company_id = company_id[1]
@@ -42,22 +41,40 @@ class CompanyView(View):
             
             if x_wanted_language not in data['company_name']:
                 x_wanted_language = list(data['company_name'].keys())[0]
-
             company_list      = {
                 'company_name' : data['company_name'][x_wanted_language],
                 'tags'         : [tag['tag_name'][x_wanted_language] for tag in data['tags']]
             }
-            
             return JsonResponse(company_list, status = 201)
-        
         except KeyError:
             JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+            
+    def get(self, request):
+        try:
+            query = request.GET.get('query', '')
+            x_wanted_language=request.headers.get("x-wanted-language")
+            language_id = Language.objects.get(name=x_wanted_language).id
+            company_filter=Q()
+            if query:
+                company_filter.add(Q(name__contains = query), company_filter.AND)
+            company_list = [company.id for company in Company.objects.filter(company_filter)]
+            companies = [Company.objects.filter(company_filter, companylanguage__language_id=language_id)]
+            if not companies[0]:
+                companies = [Company.objects.filter(related_company__company_id=company_id, companylanguage__language_id=language_id) for company_id in company_list]
+            searched_companies = [{
+                'company_name' : com.name,
+            }for company in companies for com in company]
+            return JsonResponse({'searched_companies' : searched_companies}, status=200)
+        except ValueError:
+            return JsonResponse({'message' : 'VALUE_ERROR'}, status = 400)
+        except Language.DoesNotExist:
+            return JsonResponse({'message' : 'LANGUAGE_DOES_NOT_EXIST'}, status = 400)
             
 class CompanySearchView(View):
     def get(self, request, company_name):
         try:
-            language = Language.objects.get(name=request.headers['x-wanted-language'])
-            company  = Company.objects.get(name=company_name) 
+            language = Language.objects.get(name=request.headers.get('x-wanted-language'))
+            company  = Company.objects.get(name=company_name)
 
             if CompanyLanguage.objects.get(company_id=company.id).language_id != language.id:
                 company_info = Company.objects.get(related_company__company_id=company.id, companylanguage__language_id=language.id)
