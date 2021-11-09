6a2aed47dc71d5fa82c78033aa96100354ad445c
thisisempty <thisisemptyyy@gmail.com>
2021-11-09 15:10:42 +0900

    [CREATE] initial settings

---
+++ company/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Language(models.Model):
+    name = models.CharField(max_length=45, unique=True)
+
+    class Meta:
+        db_table = 'languages'
+
+class Tag(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'tags'
+
+class Company(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'companies'
+
+class CompanyLanguageTag(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'companies_languages_tags'
