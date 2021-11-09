9e65eda0736ff3b8253d722403e31e381ce042b0
qhsdnr0 <qhsdnr0@gmail.com>
2021-11-09 17:39:26 +0900

    Create : CompanyView(post method)
    - CompanyView 포스트 메소드 추가, 헤더값에 따라 리턴하는 언어 다르게 표시
    
    Modify : CompanyView
    
    - 모델 수정(테이블 추가)에 따른 CompanyView 수정

--- company/urls.py
+++ company/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import CompanyView
+
+urlpatterns = [
+    path('', CompanyView.as_view())
+]
--- company/views.py
+++ company/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+from django.db    import transaction
+
+from .models      import Company, Tag, Language
+
+class CompanyView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            with transaction.atomic():
+                for language in data['company_name']:
+                    languages, created = Language.objects.get_or_create(name=language)
+                    company            = Company.objects.create(
+                        name = data['company_name'][language]
+                    )
+                    languages.companylanguage_set.create(
+                        company = company
+                    )
+                    for tag in data['tags']:
+                        tags, created = Tag.objects.get_or_create(name=tag['tag_name'][language])
+                        tags.companytag_set.create(
+                            company = company
+                        )
+                        tags.taglanguage_set.create(
+                            language = languages
+                        )
+                        
+
+            x_wanted_language = request.headers.get("x-wanted-language")
+            
+            if x_wanted_language not in data['company_name']:
+                x_wanted_language = list(data['company_name'].keys())[0]
+
+            company_list      = {
+                'company_name' : data['company_name'][x_wanted_language],
+                'tags'         : [tag['tag_name'][x_wanted_language] for tag in data['tags']]
+            }
+            
+            return JsonResponse({'message' : 'CREATED', 'result' : company_list}, status = 201)
+        
+        except KeyError:
+            JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+            
+            
