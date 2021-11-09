e11bbfe43e0436ed7b63d4e54bbbea56d5b3a3c4
thisisempty <thisisemptyyy@gmail.com>
2021-11-09 17:56:47 +0900

    [CREATE] initial settings

--- company/models.py
+++ company/models.py
@@ -18,10 +18,23 @@ class Company(models.Model):
     class Meta:
         db_table = 'companies'
 
-class CompanyLanguageTag(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
+class CompanyTag(models.Model):
+    tag     = models.ForeignKey(Tag, on_delete=models.PROTECT)
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'companies_tags'
+
+class CompanyLanguage(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    company  = models.ForeignKey(Company, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'companies_languages'
+
+class TagLanguage(models.Model):
+    tag      = models.ForeignKey(Tag, on_delete=models.PROTECT)
     language = models.ForeignKey(Language, on_delete=models.PROTECT)
 
     class Meta:
-        db_table = 'companies_languages_tags'
+        db_table = 'tags_languages'
